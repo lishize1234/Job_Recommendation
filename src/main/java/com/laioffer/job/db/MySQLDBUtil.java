5968151b353e3cfcc4c30c91283c68437aa52ed0
Shize Li <54549527+lishize1234@users.noreply.github.com>
2020-09-12 11:37:21 -0700

    Update MySQLDBUtil.java

--- src/main/java/com/laioffer/job/db/MySQLDBUtil.java
+++ src/main/java/com/laioffer/job/db/MySQLDBUtil.java
@@ -1,11 +1,11 @@
 package com.laioffer.job.db;
 
 public class MySQLDBUtil {
-    private static final String INSTANCE = "database-mysql-job.cctvgi5apyis.us-east-2.rds.amazonaws.com";
+    private static final String INSTANCE = YOUR_OWN_PATH;
     private static final String PORT_NUM = "3306";
     public static final String DB_NAME = "Jobproject";
-    private static final String USERNAME = "admin";
-    private static final String PASSWORD = "li132999";
+    private static final String USERNAME = YOUR_USER;
+    private static final String PASSWORD = YOUR_PASSWORD;
     public static final String URL = "jdbc:mysql://"
             + INSTANCE + ":" + PORT_NUM + "/" + DB_NAME
             + "?user=" + USERNAME + "&password=" + PASSWORD
